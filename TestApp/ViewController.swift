a549f30318a5e4032a38a2e9bfc4f303509c927b
Pushpinder Kaur <Pushpinder.Kaur@rci.rogers.com>
2018-03-19 15:28:15 -0400

    Test Project Setup for Xcode Server Continuous Integration

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Pushpinder Kaur on 2018-03-08.
+//  Copyright © 2018 Pushpinder Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
